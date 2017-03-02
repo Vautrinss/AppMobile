810b0e52e9d93c316d11c41d8c1432b6ce72f46d
Baptiste VAUTRIN <baptiste.vautrin@igtp3-mac02.isim.intra>
2017-03-02 12:43:50 +0100

    Fil dactu en très bonne voie, aucune erreur dans ce commit

---
+++ DialogBoxHelper.swift
@@ -0,0 +1,26 @@
+//
+//  DialogBoxHelper.swift
+//  IOS Project
+//
+//  Created by Baptiste VAUTRIN on 02/03/2017.
+//  Copyright © 2017 leobaptiste. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DialogBoxHelper{
+    
+    class func alert(view: UIViewController, WithTitle title:String, andMessage msg: String = "") {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title:"Ok", style: .default)
+        alert.addAction(cancelAction)
+        view.present(alert, animated: true)
+        
+    }
+        class func alert(view: UIViewController, error: NSError){
+            self.alert(view: view, WithTitle: "\(error)", andMessage: "\(error.userInfo)")
+        }
+    
+    
+}
