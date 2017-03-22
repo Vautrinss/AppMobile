0589522ef7f6a2d0c50bda8eb08ab8ce290c1c01
Leo PERNELLE <leo.pernelle@igtp3-mac013.isim.intra>
2017-03-22 09:52:51 +0100

    hgf

--- ProfilController.swift
+++ ProfilController.swift
@@ -10,23 +10,54 @@ import Foundation
 import UIKit
 import CoreData
 
-class ProfilController: UIViewController {
+class ProfilController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     
     @IBOutlet weak var lastName: UITextField!
     
     @IBOutlet weak var firstName: UITextField!
     
+    @IBOutlet weak var statut: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Connect data:
+        self.statut.delegate = self
+        self.statut.dataSource = self
+        
+        // Input data into the Array:
+        pickerData = ["Etudiant", "Enseignant", "Secretaire", "Responsable departement"]
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    
+    
+    
+    
     
     @IBAction func deconnexion(_ sender: Any) {
         
