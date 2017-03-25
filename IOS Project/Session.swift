2551f22ad0a234d68860709fe6b64980dc86de68
leopardNL <leo.pernelle2@gmail.com>
2017-03-25 19:17:32 +0100

    comment

--- IOS Project/Session.swift
+++ IOS Project/Session.swift
@@ -10,18 +10,33 @@ import Foundation
 import CoreData
 
 class Session{
+    
+    // MARK: - Session Variables
+    
     static var personnes : PersonneSet = PersonneSet()
     static var userConnected : Personne?
     
+    
+     @discardableResult
+    
+    // MARK: - Session methods
+       
+    /// login : Check si le couple login-mdp correspond à un utilisateur et réfère l'utilisateur de la Session.
     
     static func login(login: String, passwd: String){
         self.userConnected = self.personnes.doesUserExist(login: login, passwd: passwd)
         let g : GroupeSet = GroupeSet()
         GroupeSet.groupeChoisi = g.groupeCorrespondant(name: "Tous")
     }
+    
+    // logout : Suppprime la session actuel
+    
     static func logout(){
         self.userConnected = nil
     }
+    
+    // isUserConnected : renvoie true si il y a une session en cours false sinon
+    
     static var isUserConnected : Bool{
         get{
             return self.userConnected != nil
