798847e19aff225c8788f181ffff265f72f2c119
bou kisrane <boukisrane@macbook-pro-de-bou.local>
2017-03-25 12:18:33 +0100

    addEvent

---
+++ IOS Project/AddEventViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddEventViewController.swift
+//  IOS Project
+//
+//  Created by bou kisrane on 25/03/2017.
+//  Copyright © 2017 leobaptiste. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import EventKit
+
+class AddEventViewController: UIViewController {
+
+    var calendar: EKCalendar!
+    
+    
+
+    
+}
